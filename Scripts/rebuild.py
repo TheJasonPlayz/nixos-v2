6bf8449324fe02bf63207e1084302426a5a01be3
Jason D Whitman <jasondwhitman1124@gmail.com>
2025-02-13 20:43:08 -0700

    Auto-commit update

--- Scripts/rebuild.py
+++ Scripts/rebuild.py
@@ -4,6 +4,7 @@ from sys import argv
 from git import Repo, RemoteProgress, Actor
 from tqdm import tqdm
 from yaml import safe_load
+from os import environ
 
 REBUILD_DIR=Path('/etc/nixos/')
 PWD=Path.cwd()
@@ -40,22 +41,25 @@ def git_pre(repo: Repo):
     repo.git.add('-A')
 
 def git_post(repo: Repo):
-    repo.index.commit(f"-m {AUTOCOMMIT_MESSAGE}", author=AUTHOR)
+    repo.git.commit(f"-m {AUTOCOMMIT_MESSAGE}", author=AUTHOR)
     repo.remote().push(progress=Progress())
 
 def rsync_func(dir1: str, dir2: str) -> None:
-    cmds = f"rsync -ru --exclude=Scripts/ --delete {dir1} {dir2}"
+    cmds = f"sudo rsync -ru --exclude=Scripts/ --delete {dir1} {dir2}"
     run(split_args(cmds), stdout=DEVNULL)
 
 def rebuild_func(hostname: str, other_flags: list[str]) -> CompletedProcess:
-    cmds = f"nixos-rebuild switch --flake {" ".join(other_flags)} /etc/nixos#{hostname}"
+    cmds = f"sudo nixos-rebuild switch --flake {" ".join(other_flags)} /etc/nixos#{hostname}"
     return run(split_args(cmds), stdout=PIPE, stderr=PIPE)
 
 def __main__():
-    git_pre(REPO)
-
     password_yaml = get_stdout(run(["sops", "-d", "./secrets.yaml"], stdout=PIPE))
     password = safe_load(password_yaml)["github"]["pac"]
+    environ["GIT_USERNAME"] = "TheJasonPlayz"
+    environ["GIT_PASSWORD"] = password
+
+    git_pre(REPO)
+
     hostname = get_stdout(run(["hostnamectl", "hostname"], stdout=PIPE))
     direction = input(f"To OR From {str(REBUILD_DIR)}?\n(*). To {str(REBUILD_DIR)}\n(1). From {str(REBUILD_DIR)}`\n")
     switch_bool = input("Switch?\n(*). Yes\n(N/n). No\n").lower()
@@ -80,5 +84,4 @@ def __main__():
     git_post(REPO)
 
     print("=== REBUILD ===", rebuild_output, sep="\n")
-    print(password)
 __main__()
