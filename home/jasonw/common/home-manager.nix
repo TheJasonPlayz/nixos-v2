88aca78955f2b7bc92b593a822689d6e0198937d
Jason D Whitman <jasondwhitman1124@gmail.com>
2025-02-15 13:58:07 -0700

    Auto-commit update

--- home/jasonw/common/home-manager.nix
+++ home/jasonw/common/home-manager.nix
@@ -9,7 +9,7 @@
     file = {
       id_ed25519 = {
         force = true;
-        source = sops.secrets."ssh/priv_key".path;
+        text = builtins.readFile sops.secrets."ssh/priv_key".path;
         target = ".ssh/id_ed25519";
       };
     };
