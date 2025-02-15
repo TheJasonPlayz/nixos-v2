a8784fceefd180910de95c1707ed6bfb7fbbc3e8
Jason D Whitman <jasondwhitman1124@gmail.com>
2025-02-15 14:07:50 -0700

    Auto-commit update

--- hosts/common/nixos.nix
+++ hosts/common/nixos.nix
@@ -3,10 +3,9 @@
 {
   system.stateVersion = "24.11";
   nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      plugin-files = "${pkgs.nix-plugins}/lib/nix/plugins";
-      extra-builtins-file = [ ./pkgs/extra-builtins.nix ];
-    };
+    extraOptions = ''
+      plugin-files = ${pkgs.nix-plugins}/lib/nix/plugins
+    '';
+    settings.experimental-features = [ "nix-command" "flakes" ];
   };
 }
