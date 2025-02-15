5d2eb13dd62f747d1f00147ceae610fa322ba13f
Jason D Whitman <jasondwhitman1124@gmail.com>
2025-02-15 11:05:46 -0700

    Auto-commit update

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,5 +1,10 @@
-[
-  ./boot.nix
-  ./hardware.nix
-  ./configuration.nix
-] ++ (import ./pkgs.nix)
+{ ... }:
+
+{
+  imports = [
+    ./boot.nix
+    ./hardware.nix
+    ./configuration.nix
+    ./pkgs.nix
+  ];
+}
