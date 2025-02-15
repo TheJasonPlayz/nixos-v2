25056904c07de66d720ca4d0a57c3b4cb75fb137
Jason D Whitman <jasondwhitman1124@gmail.com>
2025-02-14 21:51:26 -0700

    Auto-commit update

--- Scripts/rebuild.py
+++ Scripts/rebuild.py
@@ -26,7 +26,7 @@ def git_post(repo: Repo):
 
 def rsync_func(dir1: str, dir2: str) -> None:
     cmds = f"echo {SUDO_PASSWORD} | sudo -S rsync -ru --exclude=Scripts/ --delete {dir1} {dir2}"
-    run(split_args(cmds), stdout=DEVNULL)
+    return run(split_args(cmds), stdout=DEVNULL)
 
 def rebuild_func(hostname: str, other_flags: list[str]) -> CompletedProcess:
     cmds = f"echo {SUDO_PASSWORD} | sudo -S sudo nixos-rebuild switch --flake {" ".join(other_flags)} /etc/nixos#{hostname}"
