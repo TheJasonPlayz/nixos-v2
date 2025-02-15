0cb19aa641f41f1feccc04625041a00b07947d4f
Jason D Whitman <jasondwhitman1124@gmail.com>
2025-02-15 13:13:39 -0700

    Auto-commit update

--- hosts/common/sops.nix
+++ hosts/common/sops.nix
@@ -5,12 +5,11 @@
     defaultSopsFile = ../../secrets.yaml;
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      sops.age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
-
-      sops.secrets = {
-        "github/pac" = {};
-        "ssh/priv_key" = {};
-      };
+      keyFile = "/home/${username}/.config/sops/age/keys.txt";
+    };
+    secrets = {
+      "github/pac" = {};
+      "ssh/priv_key" = {};
     };
   };
 }
