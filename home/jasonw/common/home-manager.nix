8df923e96648d9b6a7c43cd514151d6f4f208809
Jason Whitman <thejasonplayz@gmail.com>
2025-02-16 20:36:24 -0700

    Auto-commit update

--- home/jasonw/common/home-manager.nix
+++ home/jasonw/common/home-manager.nix
@@ -7,11 +7,11 @@
     homeDirectory = "/home/${username}";
     stateVersion = "24.11";
     file = {
-      id_ed25519 = {
-        force = true;
-        source = sops.secrets."ssh/priv_key".path;
-        target = ".ssh/id_ed25519";
-      };
+      # id_ed25519 = {
+      #   force = true;
+      #   source = sops.secrets."ssh/priv_key".path;
+      #   target = ".ssh/id_ed25519";
+      # };
     };
   };
 }
