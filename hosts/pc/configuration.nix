14488e57f9de729df6d3e577e58268462111aa48
Jason Whitman <thejasonplayz@gmail.com>
2025-02-16 20:18:24 -0700

    Auto-commit update

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -1,18 +1,5 @@
 { pkgs, ... }:
 
 {
-  services.usbmuxd.enable = true;
 
-  programs = {
-    steam.enable = true;
-    droidcam.enable = true;
-    chromium.enable = true;
-    _1password-gui.enable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    libimobiledevice
-    usbmuxd
-    razergenie
-  ];
 }
